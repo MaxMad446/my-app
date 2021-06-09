f017fbcb9b05455bec2c9cd893c49e277cd65c73
MAX <puper-1981@mail.ru>
2021-06-09 20:52:05 +0500

    ok

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
+import Todo from './components/Todo.js';
 
 function App() {
   return (
     <div>
       <h1>My Todos</h1>
-      <div className="card">
-        <h2>TITLE</h2>
-        <div className="actions">
-          <button className="btn">Delete</button>
-        </div>
-      </div>
+      <Todo text='you'/>
+      <Todo text='get'/>
+      <Todo text='more'/>
     </div>
   );
 }
