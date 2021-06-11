484729915376df01c6e9be3c3e9d5f1253b453ef
MAX <puper-1981@mail.ru>
2021-06-11 15:43:01 +0500

    site

---
+++ src/pages/AllMeetups.js
@@ -0,0 +1,35 @@
+import { useState, useEffect } from "react";
+
+import MeetupList from "../components/meetups/MeetupList";
+
+
+function AllMeetupsPage() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [loadedMeetups, setLoadedMeetups] = useState([]);
+
+  fetch("https://meetups-d13d9-default-rtdb.firebaseio.com/meetups.json")
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      setIsLoading(false);
+      setLoadedMeetups(data);
+    });
+
+  if (isLoading) {
+    return (
+      <section>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+
+  return (
+    <section>
+      <h1>All Meetups</h1>
+      <MeetupList meetups={loadedMeetups} />
+    </section>
+  );
+}
+
+export default AllMeetupsPage;
