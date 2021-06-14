c8f3de49f2d4f25ba6dd71cdb361d40c6cdb5958
MAX <puper-1981@mail.ru>
2021-06-14 21:42:25 +0300

    demo end

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -2,23 +2,32 @@ import { useState, useEffect } from "react";
 
 import MeetupList from "../components/meetups/MeetupList";
 
-
 function AllMeetupsPage() {
   const [isLoading, setIsLoading] = useState(true);
   const [loadedMeetups, setLoadedMeetups] = useState([]);
 
   useEffect(() => {
-      fetch("https://meetups-d13d9-default-rtdb.firebaseio.com/meetups.json")
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      setIsLoading(false);
-      setLoadedMeetups(data);
-    });
-  }, [setIsLoading, setLoadedMeetups]);
+    setIsLoading(true);
+    fetch("https://meetups-d13d9-default-rtdb.firebaseio.com/meetups.json")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        const meetups = [];
 
+        for (const key in data) {
+          const meetup = {
+            id: key,
+            ...data[key],
+          };
 
+          meetups.push(meetup)
+        }
+
+        setIsLoading(false);
+        setLoadedMeetups(meetups);
+      });
+  }, []);
 
   if (isLoading) {
     return (
